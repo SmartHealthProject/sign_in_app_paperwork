1d40cc9ddf00223631e706d103ff9c38958273c6
Trudy Kwok <kwokt3@gmail.com>
2024-03-09 19:44:51 -0500

    Your detailed commit message

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:application_paperwork/pages/login_page.dart';
+//import 'package:firebase_auth/firebase_auth.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+    );
+  }
+}
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:application_paperwork/components/my_button.dart';
+import 'package:application_paperwork/components/my_textfield.dart';
+
+class LoginPage extends StatelessWidget {
+  LoginPage({super.key});
+
+  //text editing controllers
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  // sign user in method
+  void signUserIn() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.grey[300],
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //logo
+            const SizedBox(height: 25),
+            //welcome back
+            Text(
+              "Welcome back!",
+              style: TextStyle(
+                color: Colors.grey[700],
+                fontSize: 16,
+              ),
+            ),
+
+            const SizedBox(height: 25),
+            //username
+            MyTextField(
+              controller: usernameController,
+              hintText: 'Username',
+              obscureText: false,
+            ),
+
+            const SizedBox(height: 10),
+            //password
+            MyTextField(
+              controller: passwordController,
+              hintText: 'Password',
+              obscureText: true,
+            ),
+
+            const SizedBox(height: 10),
+            //forgot password
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text('Forgot Password',
+                      style: TextStyle(color: Colors.grey[600])),
+                ],
+              ),
+            ),
+            const SizedBox(height: 25),
+            //sign in button
+            MyButton(
+              onTap: signUserIn,
+            ),
+
+            const SizedBox(height: 50),
+            //or continue with
+
+            //not a member, register now
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Not a member?',
+                    style: TextStyle(color: Colors.grey[700])),
+                const SizedBox(width: 4),
+                const Text('Register now',
+                    style: TextStyle(
+                        color: Colors.blue, fontWeight: FontWeight.bold)),
+              ],
+            )
+          ],
+        ));
+  }
+}
